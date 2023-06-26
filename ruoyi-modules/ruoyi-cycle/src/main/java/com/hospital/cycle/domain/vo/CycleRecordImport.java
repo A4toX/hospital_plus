d1829ecd9ede1c47c0787ba6f203727bd311946c
nnyc <nnyc@me.com>
2023-06-26 14:35:05 +0800

    拉取代码

---
+++ ruoyi-modules/ruoyi-cycle/src/main/java/com/hospital/cycle/domain/vo/CycleRecordImport.java
@@ -0,0 +1,24 @@
+package com.hospital.cycle.domain.vo;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class CycleRecordImport implements Serializable {
+
+    /**
+     * 科室名称
+     */
+    @ExcelProperty(index = 0)
+    private String deptName;
+
+    /**
+     * 时间和学生
+     */
+    private List<String> userNameList;
+}
